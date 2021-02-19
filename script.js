2148f5dcebf1165f271cecf230c329514841ceff
gabriel20x <gabrielgarciatorresm3@gmail.com>
2021-02-19 19:45:42 -0400

    Arreglado hover y cambio multiple de colores

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let divs=[];
+let red=0,green=0,blue=0;
 const contenedor = document.querySelector("#contenedor");
-const colores = ["#ff00ff","#800000","#66cdaa","#0000cd","#dc143c","#00ffff"];
 
 function cambiarcolor(celda){
-    divs[celda].style.backgroundColor='blue';
+    red = Math.floor(Math.random() * (255.99));
+    green = Math.floor(Math.random() * (255.99));
+    blue = Math.floor(Math.random() * (255.99));
+    divs[celda].style.backgroundColor = `rgb(${red},${green},${blue})`;
 }
 
 for (let celda = 0; celda < (16*16); celda++) {
     divs.push(document.createElement("div"));
     divs[celda].setAttribute('id', `div${celda}`);
     divs[celda].setAttribute('class', 'celda');
-    divs[celda].addEventListener("hover",cambiarcolor(celda))
+    divs[celda].addEventListener('mouseover', 
+        function () {
+        cambiarcolor(celda);
+        });
     contenedor.appendChild(divs[celda]);
 }
